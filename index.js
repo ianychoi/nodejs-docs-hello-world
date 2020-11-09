a423cbc3a0ac2759d3c480efb420ef0f488e55f2
Ian Y. Choi <ianyrchoi@gmail.com>
2020-11-10 00:15:50 +0900

    index.js: revert to a sample hello message

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello CNCG 2020 Seoul! This is the last track 3 session!!");
+    response.end("Hello, nodejs-docs!");
 });
 
 const port = process.env.PORT || 1337;
