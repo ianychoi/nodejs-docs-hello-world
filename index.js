da45c4dfaeb4f8a5a5be557bb89f1e917ab488a4
inhye park <cosmos0703@gmail.com>
2020-11-30 08:15:24 +0900

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello, Azure DevOps Summit participants!");
+    response.end("Hello, This is the CNCG!");
 });
 
 const port = process.env.PORT || 1337;
