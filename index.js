48d7085e5563e6aba0bc1948eb91fb478a4372b1
inhye park <cosmos0703@gmail.com>
2020-11-30 05:21:00 +0000

    2 index print updated

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("####### Hello, This is the CSU session!");
+    response.end("####### Hello, This is the CSU session!12333333");
 });
 
 const port = process.env.PORT || 1337;
